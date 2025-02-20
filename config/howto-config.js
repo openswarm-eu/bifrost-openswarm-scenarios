d6c6999bc7637ce167ea7eccc73c7ab2a75bbd8b
Franz Zeilinger <franz.zeilinger@siemens.com>
2025-02-20 09:33:46 +0100

    Initial commit (based on HowTo_Docker from DevHub)

---
+++ config/howto-config.js
@@ -0,0 +1,10 @@
+export const userConfig = {
+    plugins: {
+        poweredElementsStructDynMap: {
+            'POWERGRID-CONNECTOR': 'ACTIVE-POWER',
+            'LV-TRANSFORMER': 'ACTIVE-POWER',
+            'COMMERCIAL-BATTERY-SMALL': 'STATE-OF-CHARGE',
+            'COMMERCIAL-BATTERY-LARGE': 'STATE-OF-CHARGE'
+        }
+    }
+}
