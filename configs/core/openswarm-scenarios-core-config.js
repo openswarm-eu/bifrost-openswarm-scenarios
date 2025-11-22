7809bee8ea373376bc537d86688a78bf375b55e3
Franz Zeilinger <franz.zeilinger@siemens.com>
2025-11-22 20:27:25 +0100

    FIX: poweredElementsStructDynMap for accurate structure illumination

--- configs/core/openswarm-scenarios-core-config.js
+++ configs/core/openswarm-scenarios-core-config.js
@@ -1,16 +1,13 @@
 export const userConfig = {
     plugins: {
         poweredElementsStructDynMap: {
-            'POWERGRID-CONNECTOR': 'VOLTAGE-3P',
-            'LV-TRANSFORMER': 'TRAFO-VOLTAGE',
-            'COMMERCIAL-BATTERY-SMALL': 'VOLTAGE-3P',
-            'COMMERCIAL-BATTERY-LARGE': 'VOLTAGE-3P'
+            'POWERGRID-CONNECTOR' : 'VOLTAGE-3P',
+            'LV-TRANSFORMER'      : 'TRAFO-VOLTAGE',
         },
         poweredDetailsStructDynMap: {
                     'CHARGING-POLE': {
-                        'SLOT-VISIBLE-ARRAY': 'EV-STATION-SOC',
-                        'SLOT-COLOR-ARRAY': 'EV-STATION-SLOT-COLOR'
-
+                        'SLOT-VISIBLE-ARRAY' : 'EV-STATION-SOC',
+                        'SLOT-COLOR-ARRAY'   : 'EV-STATION-SLOT-COLOR'
                     }
                 },
         additionalPlugins: ['PoweredDetailsPlugin.js']
